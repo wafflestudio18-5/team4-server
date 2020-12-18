fd2239a5cd6e5c3f5abdc9557d050661da16eca2
YeonghyeonKO <dk02315@gmail.com>
2020-12-18 17:42:19 +0900

    fix typo and include migration files

--- wafflow/comment/models.py
+++ wafflow/comment/models.py
@@ -18,7 +18,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, related_name="commments", on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name="comments", on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, related_name="comments", on_delete=models.CASCADE)
     vote = models.IntegerField(default=0)
 
