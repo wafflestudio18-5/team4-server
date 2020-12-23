5cb569ab58cb3ac82ac58ef3682178e5bf13b4bd
guzus <50664161+guzus@users.noreply.github.com>
2020-12-23 23:09:48 +0900

    in progress

--- wafflow/rate/views.py
+++ wafflow/rate/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from question.models import UserQuestion, Question
+from answer.models import UserAnswer, Answer
+from comment.models import UserComment, Comment
+
+
+class RateViewSet(viewsets.GenericViewSet):
+    permission_classes = (IsAuthenticated(), )
+
+    def get_permissions(self):
+        return self.permission_classes
+    
+    # PUT /rate/question/{question_id}
+    def rate_question(self, request, pk=None):
+        user = request.user
+        try:
+            question = Question.objects.get_object_or_404(id=pk)
+        except Question.DoesNotExist:
+            return Response(
+                {"message": "There is no question with the id"}, status=status.HTTP_404_NOT_FOUND,
+            )
+        userquestion = user.userquestion.get(question=question)
+        
+        rating = request.data.get('rating')
+        question.vote += rating - userquestion.rating
+        userquestion.rating = rating
+
+        data = {
+            "user_id": user.id,
+            "question_id": question.id,
+            "vote": question.vote,
+            "rating": rating
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    # PUT /rate/answer/{answer_id}
+    def rate_answer(self, request, pk=None):
+        pass
+    # PUT /rate/comment/{comment_id}
+    def rate_comment(self, request, pk=None):
+        pass
