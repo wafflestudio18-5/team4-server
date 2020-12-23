a1cc3f71602d4a84d6980a1020722d42b084a5bc
guzus <50664161+guzus@users.noreply.github.com>
2020-12-24 03:09:02 +0900

    fix bug

--- wafflow/rate/views.py
+++ wafflow/rate/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import status, viewsets
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -10,23 +10,30 @@ from comment.models import UserComment, Comment
 
 
 class RateViewSet(viewsets.GenericViewSet):
-    permission_classes = (IsAuthenticated(), )
+    permission_classes = (IsAuthenticated(),)
 
     def get_permissions(self):
         return self.permission_classes
-    
+
     # PUT /rate/question/{question_id}
-    def rate_question(self, request, pk=None):
+    @api_view(("PUT",))
+    def rate_question(request, pk):
         user = request.user
         try:
-            question = Question.objects.get_object_or_404(id=pk)
+            question = Question.objects.get(id=pk)
         except Question.DoesNotExist:
             return Response(
-                {"message": "There is no question with the id"}, status=status.HTTP_404_NOT_FOUND,
+                {"message": "There is no question with the id"},
+                status=status.HTTP_404_NOT_FOUND,
             )
-        userquestion = user.user_questions.objects.get(question=question)
-        
-        rating = request.data.get('rating')
+        try:
+            userquestion = user.user_questions.get(question=question)
+        except:
+            userquestion = UserQuestion.objects.create(
+                user=user, question=question, rating=UserQuestion.NONE
+            )
+
+        rating = int(request.data.get("rating"))
         question.vote += rating - userquestion.rating
         userquestion.rating = rating
 
@@ -34,22 +41,29 @@ class RateViewSet(viewsets.GenericViewSet):
             "user_id": user.id,
             "question_id": question.id,
             "vote": question.vote,
-            "rating": rating
+            "rating": rating,
         }
         return Response(data, status=status.HTTP_200_OK)
 
     # PUT /rate/answer/{answer_id}
-    def rate_answer(self, request, pk=None):
+    @api_view(("PUT",))
+    def rate_answer(request, pk):
         user = request.user
         try:
-            answer = Answer.objects.get_object_or_404(id=pk)
+            answer = Answer.objects.get(id=pk)
         except Answer.DoesNotExist:
             return Response(
-                {"message": "There is no answer with the id"}, status=status.HTTP_404_NOT_FOUND,
+                {"message": "There is no answer with the id"},
+                status=status.HTTP_404_NOT_FOUND,
             )
-        useranswer = user.user_answers.objects.get(answer=answer)
-        
-        rating = request.data.get('rating')
+        try:
+            useranswer = user.user_answers.get(answer=answer)
+        except:
+            useranswer = UserAnswer.objects.create(
+                user=user, answer=answer, rating=UserAnswer.NONE
+            )
+
+        rating = int(request.data.get("rating"))
         answer.vote += rating - useranswer.rating
         useranswer.rating = rating
 
@@ -57,22 +71,29 @@ class RateViewSet(viewsets.GenericViewSet):
             "user_id": user.id,
             "answer_id": answer.id,
             "vote": answer.vote,
-            "rating": rating
+            "rating": rating,
         }
-        return Response(data, status=status.HTTP_200_OK)   
+        return Response(data, status=status.HTTP_200_OK)
 
     # PUT /rate/comment/{comment_id}
-    def rate_comment(self, request, pk=None):
+    @api_view(("PUT",))
+    def rate_comment(request, pk):
         user = request.user
         try:
-            comment = Comment.objects.get_object_or_404(id=pk)
+            comment = Comment.objects.get(id=pk)
         except Comment.DoesNotExist:
             return Response(
-                {"message": "There is no comment with the id"}, status=status.HTTP_404_NOT_FOUND,
+                {"message": "There is no comment with the id"},
+                status=status.HTTP_404_NOT_FOUND,
             )
-        usercomment = user.user_comments.objects.get(comment=comment)
-        
-        rating = request.data.get('rating')
+        try:
+            usercomment = user.user_comments.get(comment=comment)
+        except:
+            usercomment = UserComment.objects.create(
+                user=user, comment=comment, rating=UserComment.NONE
+            )
+
+        rating = int(request.data.get("rating"))
         comment.vote += rating - usercomment.rating
         usercomment.rating = rating
 
@@ -80,6 +101,6 @@ class RateViewSet(viewsets.GenericViewSet):
             "user_id": user.id,
             "comment_id": comment.id,
             "vote": comment.vote,
-            "rating": rating
+            "rating": rating,
         }
         return Response(data, status=status.HTTP_200_OK)
