f75d3772eec832a8958d7ccccfee81ac064e9f52
guzus <50664161+guzus@users.noreply.github.com>
2020-12-25 17:11:58 +0900

    large update

--- wafflow/user/views.py
+++ wafflow/user/views.py
@@ -6,26 +6,25 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from user.serializers import UserSerializer, UserProfileSerializer
+from user.serializers import UserSerializer, UserProfileSerializer, AuthorSerializer
 
 from user.models import UserProfile
-from question.models import UserQuestion
-from answer.models import UserAnswer
+from question.models import UserQuestion, Question
+from answer.models import UserAnswer, Answer
 
 
 class UserViewSet(viewsets.GenericViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
     permission_classes = (IsAuthenticated(),)
 
     def get_permissions(self):
-        if self.action in ("create", "login"):
+        if self.action in ("create", "login", "retrieve"):
             return (AllowAny(),)
         return self.permission_classes
 
     def get_serializer_class(self):
-        if self.action == ("create", "update"):
-            return self.serializer_class
+        if self.action in ("create", "update"):
+            return UserSerializer
         return UserProfileSerializer
 
     def create(self, request):
@@ -35,9 +34,7 @@ class UserViewSet(viewsets.GenericViewSet):
 
         login(request, user)
 
-        serializer = UserProfileSerializer(user.profile, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data = serializer.data
+        data = UserProfileSerializer(user.profile).data
         data["token"] = user.auth_token.key
         return Response(data, status=status.HTTP_201_CREATED)
 
@@ -50,7 +47,7 @@ class UserViewSet(viewsets.GenericViewSet):
         if user and user.is_active == True:
             login(request, user)
 
-            data = get_serializer(user.profile).data
+            data = self.get_serializer(user.profile).data
             token, created = Token.objects.get_or_create(user=user)
             data["token"] = token.key
             return Response(data)
@@ -66,10 +63,18 @@ class UserViewSet(viewsets.GenericViewSet):
 
     def retrieve(self, request, pk=None):
         if pk == "me":
+            if not request.user.is_authenticated:
+                return Response(
+                    {"message": "Invalid Token"}, status=status.HTTP_401_UNAUTHORIZED
+                )
             user = request.user
             data = self.get_serializer(user.profile).data
-            data["answer_count"] = user.user_answers.count()
-            data["bookmark_count"] = user.user_questions.filter(bookmark=True).count()
+            data["answer_count"] = Answer.objects.filter(
+                user=user, is_active=True
+            ).count()
+            data["bookmark_count"] = user.user_questions.filter(
+                bookmark=True, question__is_active=True
+            ).count()
             return Response(data)
         else:
             user = self.get_object()
@@ -85,14 +90,11 @@ class UserViewSet(viewsets.GenericViewSet):
         user = request.user
         data = request.data
 
-        serializer = self.get_serializer(user, data=data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.update(user, serializer.validated_data)
+        serializer = self.get_serializer(user, data=data)
+        serializer.is_valid()
+        serializer.save()
 
-        serializer = UserProfileSerializer(user.profile, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        data = serializer.data
-
+        data = UserProfileSerializer(user.profile).data
         return Response(data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk=None):
@@ -105,6 +107,7 @@ class UserViewSet(viewsets.GenericViewSet):
         user = request.user
         if user.is_active == True:
             user.is_active = False
+            user.save()
             logout(request)
             return Response(status=status.HTTP_200_OK)
         else:
