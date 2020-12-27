6aa32f4f384ce5cef58e393f3508bb8fc9ccbb50
guzus <50664161+guzus@users.noreply.github.com>
2020-12-28 00:34:17 +0900

    profile image

--- wafflow/wafflow/urls.py
+++ wafflow/wafflow/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,4 +12,4 @@ urlpatterns = [
     path("", include("user.urls")),
     path("", include("bookmark.urls")),
     path("", include("rate.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
