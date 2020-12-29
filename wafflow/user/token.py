e4df27bbc203cdaaecf2fa98318295b22ab4194f
eldpswp99 <nrg1392@naver.com>
2020-12-29 23:35:00 +0900

    fix github token

--- wafflow/user/token.py
+++ wafflow/user/token.py
@@ -3,9 +3,7 @@ from rest_framework import status
 from user.constants import GITHUB_URL
 
 
-def get_github_data(data):
-    github_token = data.get("github_token")
-
+def get_github_data(github_token):
     headers = {
         "Content-Type": "application/json;",
         "Authorization": f"token {github_token}",
