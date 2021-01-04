fa0d00c40347e602f2663d34ebd5458f9e2d20f8
guzus <50664161+guzus@users.noreply.github.com>
2021-01-04 13:43:56 +0900

    init

--- wafflow/answer/admin.py
+++ wafflow/answer/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Answer, UserAnswer
+
+
+class UserAnswerInline(admin.TabularInline):
+    model = UserAnswer
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {"fields": ["user", "question", "content", "is_active"]})]
+    inlines = [
+        UserAnswerInline,
+    ]
+    list_display = (
+        "content",
+        "user",
+        "is_accepted",
+        "vote",
+        "created_at",
+        "is_active",
+    )
+    list_filter = ["created_at", "is_accepted", "updated_at", "vote", "is_active"]
+    search_fields = ["content"]
--- wafflow/user/admin.py
+++ wafflow/user/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import User, UserProfile
+from question.models import UserQuestion
+from answer.models import UserAnswer
+from comment.models import UserComment
+
+
+class UserProfileInline(admin.TabularInline):
+    model = UserProfile
+
+
+class UserQuestionInline(admin.TabularInline):
+    model = UserQuestion
+
+
+class UserAnswerInline(admin.TabularInline):
+    model = UserAnswer
+
+
+class UserCommentInline(admin.TabularInline):
+    model = UserComment
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [
+        UserProfileInline,
+        UserQuestionInline,
+        UserAnswerInline,
+        UserCommentInline,
+    ]
+
+    list_display = (
+        "username",
+        "email",
+        "is_staff",
+        "profile",
+        "is_active",
+    )
+    list_filter = ["is_staff", "is_active", "is_superuser"]
+    search_fields = ("username",)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
